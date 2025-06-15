4e836a2b4506e99eb94c658dc286e0f3adb9a4d5
Arkadiusz Szczepkowicz <arek.szczepkowicz@ng-analytics.com>
2025-06-15 10:08:51 +0200

    Add conda recipe for the package

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+set -e
+
 pushd ..
 mkdir -p build
-pushd build || exit
+pushd build
 
-cmake .. -DCMAKE_INSTALL_PREFIX="$PREFIX"
+: "${SRC_DIR:=..}"
+: "${PYTHON:=$(which python3)}"
+
+cmake "$SRC_DIR" \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+    -DPYTHON_EXECUTABLE="$PYTHON"
 
 make -j"$(nproc)"
+make install
